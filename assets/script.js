a90784467b9d495f805695cb4d42b0841d1ce835
albenchris <albenchris00@gmail.com>
2021-01-03 14:44:22 -0600

    created array of questions with answers

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,59 @@ var checkAnswerButton = document.querySelector("#check-answer");
 var resultsWrapper = document.querySelector("#results");
 
 // create variable to hold questions and answers
+var quizQuestions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        answers: {
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        },
+        correctAnswer: "alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ______.",
+        answers: {
+            "quotes",
+            "curly brackets",
+            "parenthesis",
+            "square brackets"
+        },
+        correctAnswer: "curly brackets"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        answers: {
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of these"
+        },
+        correctAnswer: "all of these"
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answers: {
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parenthesis"
+        },
+        correctAnswer: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            "JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log"
+        },
+        correctAnswer: "console.log"
+    }
+];
+
 
 // VARIABLES END
 
@@ -14,13 +67,37 @@ var resultsWrapper = document.querySelector("#results");
 // FUNCTION DEFINITIONS
 
 // create a function to start the quiz with a timer
+function startQuiz() {
+
+};
+
 // create functions to display questions in a slideshow format
+function formatQuestions() {
+
+};
+
 // create a funtion to subtract time from the clock when user selects wrong answer
+function rightAnswer() {
+
+};
+
+function wrongAnswer() {
+
+};
+
 // create a funtion to end quiz when all questions answered or timer reaches 0
+function endQuiz() {
+
+};
+
 // create a funtion to save timer value as high score with user's initials
+function highScore() {
+
+};
 
 // FUNCTION DEFINITIONS END
 
+startQuiz();
 
 
 
