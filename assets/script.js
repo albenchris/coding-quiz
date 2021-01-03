bfe65a574bc4260c3fe3761adebd9a7a8536e462
albenchris <albenchris00@gmail.com>
2021-01-03 12:54:29 -0600

    pseudocode

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+// VARIABLES
+var quizWrapper = document.querySelector("#quiz-wrapper");
+var checkAnswerButton = document.querySelector("#check-answer");
+var resultsWrapper = document.querySelector("#results");
+
+// create variable to hold questions and answers
+
+// VARIABLES END
+
+
+
+
+
+// FUNCTION DEFINITIONS
+
+// create a function to start the quiz with a timer
+// create functions to display questions in a slideshow format
+// create a funtion to subtract time from the clock when user selects wrong answer
+// create a funtion to end quiz when all questions answered or timer reaches 0
+// create a funtion to save timer value as high score with user's initials
+
+// FUNCTION DEFINITIONS END
+
+
+
+
+
+// EVENT LISTENERS
+
+
+
+// EVENT LISTENERS END
